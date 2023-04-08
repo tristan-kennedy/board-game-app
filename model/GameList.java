b8179ba79be9f67cbf4b52df3bdd446b3c07e4e6
Shrub8 <stevenwalter000@gmail.com>
2023-04-08 17:13:36 -0500

    GameList sorting transferred to GameListView

--- model/GameList.java
+++ model/GameList.java
@@ -1,7 +1,6 @@
 package model;
 
 import java.util.ArrayList;
-import java.util.Comparator;
 import java.util.HashMap;
 import java.util.Iterator;
 
@@ -28,38 +27,39 @@ public class GameList implements Iterable<Game> {
         gameMap.put(g.getID(), g);
     }
 
-    /**
-     * Returns a new list of Games sorted by either name, rating, or max players
-     *
-     * @param sortBy   The field to sort the Games by: "name", "rating", or "players"
-     * @param ordering 1 for ascending, -1 for descending
-     * @return a new, sorted list of Games
-     */
-    public ArrayList<Game> sort(String sortBy, int ordering) {
-        ArrayList<Game> sortedGames = new ArrayList<>(gameList);
-
-        switch (sortBy) {
-            case "name" -> {
-                if (ordering == 1) // Sort lexicographically
-                    sortedGames.sort(Comparator.comparing(Game::getName));
-                if (ordering == -1) // Sort reverse lexicographically
-                    sortedGames.sort(Comparator.comparing(Game::getName).reversed());
-            }
-            case "rating" -> {
-                if (ordering == 1)
-                    sortedGames.sort((a, b) -> Float.compare(a.getRating(), b.getRating()));
-                if (ordering == -1)
-                    sortedGames.sort((a, b) -> Float.compare(b.getRating(), a.getRating()));
-            }
-            case "players" -> {
-                if (ordering == 1)
-                    sortedGames.sort(Comparator.comparingInt(Game::getMaxPlayers));
-                if (ordering == -1)
-                    sortedGames.sort(Comparator.comparingInt(Game::getMaxPlayers).reversed());
-            }
-        }
-        return sortedGames;
-    }
+    // Sort Functionality not needed
+//    /**
+//     * Returns a new list of Games sorted by either name, rating, or max players
+//     *
+//     * @param sortBy   The field to sort the Games by: "name", "rating", or "players"
+//     * @param ordering 1 for ascending, -1 for descending
+//     * @return a new, sorted list of Games
+//     */
+//    public ArrayList<Game> sort(String sortBy, int ordering) {
+//        ArrayList<Game> sortedGames = new ArrayList<>(gameList);
+//
+//        switch (sortBy) {
+//            case "name" -> {
+//                if (ordering == 1) // Sort lexicographically
+//                    sortedGames.sort(Comparator.comparing(Game::getName));
+//                if (ordering == -1) // Sort reverse lexicographically
+//                    sortedGames.sort(Comparator.comparing(Game::getName).reversed());
+//            }
+//            case "rating" -> {
+//                if (ordering == 1)
+//                    sortedGames.sort((a, b) -> Float.compare(a.getRating(), b.getRating()));
+//                if (ordering == -1)
+//                    sortedGames.sort((a, b) -> Float.compare(b.getRating(), a.getRating()));
+//            }
+//            case "players" -> {
+//                if (ordering == 1)
+//                    sortedGames.sort(Comparator.comparingInt(Game::getMaxPlayers));
+//                if (ordering == -1)
+//                    sortedGames.sort(Comparator.comparingInt(Game::getMaxPlayers).reversed());
+//            }
+//        }
+//        return sortedGames;
+//    }
 
     /**
      * Finds a specific game by ID. Returns null if game not found
