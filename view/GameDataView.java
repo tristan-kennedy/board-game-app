6f44541f056006b9a745b4e83e867cc58fb8e95d
Tristan Kennedy <tdouglaskennedy@gmail.com>
2023-04-08 13:11:22 -0500

    GameDataView form without reviews

--- view/GameDataView.java
+++ view/GameDataView.java
@@ -1,21 +1,52 @@
 package view;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableModel;
 
 import model.Game;
+
+import java.awt.*;
+import java.net.URL;
 
 public class GameDataView extends JPanel {
 
     private JPanel gameDataPanel;
-    private JTextArea textArea1;
+    private JLabel gameName;
+    private JScrollPane scrollPane;
+    private JLabel imageLabel;
+    private JTextPane infoTextPane;
+    private JTable table1;
 
     public GameDataView() {
+
     }
 
-    public JPanel getPanel() { return gameDataPanel; }
-
-    public void setGame(Game g){
-        textArea1.setText(g.toString());
+    public JPanel getPanel() {
+        return gameDataPanel;
     }
 
+    public void setGame(Game g) {
+
+        gameName.setText(g.getName());
+
+        ImageIcon imageIcon = null;
+        try {
+            URL url = new URL(g.getFullSizeImage());
+            Image image = ImageIO.read(url).getScaledInstance(-1, 600, Image.SCALE_FAST);
+            imageIcon = new ImageIcon(image);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        infoTextPane.setText("Player Count: " + g.getMinPlayers() + " - " + g.getMaxPlayers() + "\nPlaying Time: " + g.getPlayingTime() + " min" + "\nYear Published: " + g.getYearPublished());
+
+        imageLabel.setIcon(imageIcon);
+
+        DefaultTableModel tableModel = new DefaultTableModel();
+        tableModel.addColumn("Hello");
+        tableModel.addColumn("Test");
+        table1.setModel(tableModel);
+    }
 }
