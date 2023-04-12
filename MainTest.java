ec094170c005879bea69605c6588cce40bf6784d
Shrub8 <stevenwalter000@gmail.com>
2023-04-12 14:22:53 -0500

    Removed old test code

--- MainTest.java
+++ MainTest.java
@@ -2,7 +2,6 @@ import model.*;
 import view.MainView;
 
 import javax.swing.*;
-import java.util.Random;
 
 public class MainTest {
     public static void main(String[] args) {
@@ -12,23 +11,11 @@ public class MainTest {
 
         UserDataManager.initializeFile();
 
-        /*
-        Random rand = new Random(System.currentTimeMillis());
-        for (int i = 0; i < 10; i++)
-            for (Game g : mainList)
-                g.addReview(new Review(rand.nextInt(10), "", g.getID(), ""));
-         */
-
         UserDataManager.createAccount("TestUser", "abc123");
 
         UserDataManager.login("TestUser", "abc123");
 
         UserDataManager.loadReviews();
-
-//        MainView main = new MainView(mainList);
-//
-//        main.changeGameView(mainList.getGame(381247));
-//        main.changeGameView(mainList.getGame(374173));
 
         JFrame frame = new JFrame("Board Game App");
         frame.setContentPane(new MainView(GameDatabaseLoader.mainList).getMainPanel());
